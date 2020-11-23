5b018ddfcee67beec6e60d896352c4f66cb7c9b5
Pedro Henrique Zambelli <zambr321@gmail.com>
2020-11-23 18:21:21 -0300

    Exercicio Composição C# - Contratos

---
+++ Treino/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace Treino.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Treino/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Treino.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
